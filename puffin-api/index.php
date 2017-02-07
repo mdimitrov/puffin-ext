270e5136c2b0e12409ce0096ee446f07a2fb0e86
Mihael Dimitrov <mihael.dimitrov@trading212.com>
2017-02-07 21:26:44 +0200

    add /api/login

--- puffin-api/index.php
+++ puffin-api/index.php
@@ -3,12 +3,36 @@ require __DIR__ . '/vendor/autoload.php';
 
 // Your App
 $app = new Bullet\App();
-$app->path('/', function($request) use ($app) {
-    $app->get(function() {
-        return "Hello World!";
+
+/**
+ * /api
+ */
+$app->path('api', function($request) use ($app) {
+    /**
+     * /api/login
+     */
+    $app->path('login', function($request) use ($app) {
+
+         // POST /api/login
+        $app->post(function($request) use ($app) {
+            $userId = $request->username;
+            $pass = $request->password;
+
+            if ($userId !== 'atanas' || $pass !== '123abv') {
+                return $app->response(401, [
+                    'message' => 'Invalid username or password'
+                ]);
+            } else {
+                return [
+                    'status' => 'ok',
+                    'user' => [ 'username' => $userId ],
+                ];
+            }
+
+        });
     });
 });
-  
+
 // Run the app! (takes $method, $url or Bullet\Request object)
 echo $app->run(new Bullet\Request());
 
